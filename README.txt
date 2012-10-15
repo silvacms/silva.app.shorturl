be8a7e8f1d25ea37e1527608b9d8519dcc5afb06
Antonin Amand <antonin.amand@gmail.com>
2012-10-15 18:40:08 +0200

    import

---
+++ README.txt
@@ -0,0 +1,16 @@
+=================
+silva.app.shorturl
+=================
+
+Introduction
+============
+
+``silva.app.shorturl`` provide short URL for silva objects e.g:
+
+http://infr.ea/Short
+
+Code repository
+===============
+
+Source code can be found at :
+https://hg.infrae.com/silva.app.shorturl
---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2002-2011 Infrae. All rights reserved.
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+import os
+
+version = '3.0'
+
+tests_require = [
+    'Products.Silva [test]',
+    ]
+
+setup(name='silva.app.shorturl',
+      version=version,
+      description="Short url generation for silva",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silva shorturl',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='http://infrae.com/products/silva',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['silva', 'silva.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'Zope2',
+        'five.grok',
+        'grokcore.chameleon',
+        'setuptools',
+        'silva.core.conf',
+        'silva.core.services'
+        ],
+      tests_require=tests_require,
+      extras_require={'test': tests_require},
+      )
